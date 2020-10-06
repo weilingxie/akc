0ca7983b1e4ea6c11794efac08171ea7ef26f8bb
Weiling Xie <sqlnever@gmail.com>
2020-10-06 23:24:07 +1300

    Updates

---
+++ precache-manifest.e29db02121d5916ce566c4276217f199.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40765bb6e363714a0a0c73b049bc5c1a",
+    "url": "/akc/index.html"
+  },
+  {
+    "revision": "926fde084b4224059336",
+    "url": "/akc/static/css/main.c7d00279.chunk.css"
+  },
+  {
+    "revision": "ccd3af5e60c0ebe789ef",
+    "url": "/akc/static/js/2.b95f2441.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/akc/static/js/2.b95f2441.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "926fde084b4224059336",
+    "url": "/akc/static/js/main.a1098e9a.chunk.js"
+  },
+  {
+    "revision": "8876727ba73ed11cd777",
+    "url": "/akc/static/js/runtime-main.ba03f860.js"
+  }
+]);
