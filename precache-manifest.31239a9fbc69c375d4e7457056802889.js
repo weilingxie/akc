78cffd9739b042b2343adc3d1834fad136a384c9
Weiling Xie <sqlnever@gmail.com>
2020-10-12 18:31:49 +1300

    Updates

--- precache-manifest.31239a9fbc69c375d4e7457056802889.js
+++ precache-manifest.31239a9fbc69c375d4e7457056802889.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "daec445d7e1fc60170fdf9a892adbe8b",
+    "revision": "b88b6ac8164a425998890e1a17daa7c0",
     "url": "/akc/index.html"
   },
   {
-    "revision": "29ed666a3069fc52501c",
-    "url": "/akc/static/css/main.c7d00279.chunk.css"
+    "revision": "b1bc40449b3f23036916",
+    "url": "/akc/static/css/main.fe1c479c.chunk.css"
   },
   {
     "revision": "2a83bee06d453ab54491",
@@ -16,11 +16,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/akc/static/js/2.7b76f623.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "29ed666a3069fc52501c",
-    "url": "/akc/static/js/main.8bed26dc.chunk.js"
+    "revision": "b1bc40449b3f23036916",
+    "url": "/akc/static/js/main.e8d039ff.chunk.js"
   },
   {
     "revision": "8876727ba73ed11cd777",
     "url": "/akc/static/js/runtime-main.ba03f860.js"
+  },
+  {
+    "revision": "7cb3302cc32752412863e58823776e6b",
+    "url": "/akc/static/media/background2.7cb3302c.jpg"
   }
 ]);
