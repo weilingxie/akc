5e34baabb08986d23f2dffeed579cb375fc2103f
Weiling Xie <sqlnever@gmail.com>
2020-10-13 20:01:03 +1300

    Adjust Landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,7 +6,6 @@ const Landing = () => {
     return (        
         <div className="landing">
             <div className="landing-left">
-                <h1>LEFT</h1>
                 <p className="landing-title landing-title1">AUCKLAND</p>
                 <p className="landing-title landing-title2">KENDO CLUB</p>
                 <p className="landing-subtitle">Dedicated dojo</p>
@@ -14,7 +13,14 @@ const Landing = () => {
                 <button className="landing-btn">JOIN US NOW</button>
             </div>
             <div className="landing-right">
-                <h1>RIGHT</h1>
+                <iframe 
+                    title="landing-video" 
+                    className="landing-video" 
+                    src="https://www.youtube.com/embed/a-ZQBX4pnc8" 
+                    frameBorder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                    allowFullScreen>                        
+                </iframe>
             </div>            
         </div>         
     )
