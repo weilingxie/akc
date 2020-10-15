49eee306298a230d8c4949a785c737606880427c
Weiling Xie <sqlnever@gmail.com>
2020-10-16 12:06:24 +1300

    2

---
+++ build/precache-manifest.c3cffecd5153e7f00134574459190d12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4325dcad59b51b42e6714ffb45288814",
+    "url": "/akc/index.html"
+  },
+  {
+    "revision": "91203d6903e02cb1e75a",
+    "url": "/akc/static/css/main.d991f0eb.chunk.css"
+  },
+  {
+    "revision": "2a83bee06d453ab54491",
+    "url": "/akc/static/js/2.7b76f623.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/akc/static/js/2.7b76f623.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91203d6903e02cb1e75a",
+    "url": "/akc/static/js/main.3b68ab6d.chunk.js"
+  },
+  {
+    "revision": "8876727ba73ed11cd777",
+    "url": "/akc/static/js/runtime-main.ba03f860.js"
+  },
+  {
+    "revision": "7cb3302cc32752412863e58823776e6b",
+    "url": "/akc/static/media/background2.7cb3302c.jpg"
+  }
+]);
