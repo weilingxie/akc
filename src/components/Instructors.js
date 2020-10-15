7dc12b312e5a9ded2d8c6a1000e8b9803e10d2d2
Weiling Xie <sqlnever@gmail.com>
2020-10-16 11:31:55 +1300

    Instructors done

--- src/components/Instructors.js
+++ src/components/Instructors.js
@@ -5,14 +5,46 @@ const Instructors = () => {
     
     return (        
         <div className="instructors">
-            <h1 className="linstructors-title">INSTRUCTORS</h1>
-            <div className="instructors-7thDan">
-                <h3 className="instructors-dan">7th Ddan</h3>
-                <div className="instructors-card">
-                    <img className="instructor-photo" src={`${process.env.PUBLIC_URL}/images/Graham-Sayer.jpg`} alt="gramham.sayer" />
-                    <p className="instructor-name">Graham Sayer</p>
+            <h1 className="instructor-title">INSTRUCTORS</h1>
+            <h1 className="instructor-type">KENDO</h1>
+            <div className="instructor-kendo">
+                <div className="instructor-dan instructor-7thDan">
+                    <h3 className="instructor-dan-name">7th Dan</h3>
+                    <div className="instructor-card-container">
+                        <div className="instructor-card">
+                            <img className="instructor-photo" src={`${process.env.PUBLIC_URL}/images/Graham-Sayer.jpg`} alt="gramham.sayer" />
+                            <p className="instructor-name">Graham Sayer</p>
+                        </div>
+                    </div>               
+                </div>
+                <div className="instructor-dan instructor-6thDan">
+                    <h3 className="instructor-dan-name">6th Dan</h3>
+                    <div className="instructor-card-container">
+                        <div className="instructor-card">
+                            <img className="instructor-photo" src={`${process.env.PUBLIC_URL}/images/Graham-Sayer.jpg`} alt="gramham.sayer" />
+                            <p className="instructor-name">Graham Sayer</p>
+                        </div>
+                        <div className="instructor-card">
+                            <img className="instructor-photo" src={`${process.env.PUBLIC_URL}/images/Graham-Sayer.jpg`} alt="gramham.sayer" />
+                            <p className="instructor-name">Graham Sayer</p>
+                        </div>
+                        <div className="instructor-card">
+                            <img className="instructor-photo" src={`${process.env.PUBLIC_URL}/images/Graham-Sayer.jpg`} alt="gramham.sayer" />
+                            <p className="instructor-name">Graham Sayer</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="instructor-dan instructor-5thDan">
+                    <h3 className="instructor-dan-name">5th Dan</h3>
+                    <div className="instructor-card-container">
+                        <div className="instructor-card">
+                            <img className="instructor-photo" src={`${process.env.PUBLIC_URL}/images/Graham-Sayer.jpg`} alt="gramham.sayer" />
+                            <p className="instructor-name">Graham Sayer</p>
+                        </div>
+                    </div>               
                 </div>
             </div>
+            
         </div>         
     )
 }
