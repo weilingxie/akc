1e93d855646f7f94738b26c3256fbe9467106998
weilingxie1 <sqlnever@gmail.com>
2022-05-06 16:16:37 +1200

    change price and add 2 more upcoming events

--- src/components/UpcomingEvent/UpcomingEvent.js
+++ src/components/UpcomingEvent/UpcomingEvent.js
@@ -1,18 +1,28 @@
-import React from 'react'
-const Config = require('../../config.json') 
+import React from "react";
+const Config = require("../../config.json");
 
 //rfce + enter
-const UpcomingEvent = () => { 
+const UpcomingEvent = () => {
+  return (
+    <div id="upcomingEvent" className="upcomingEvent">
+      <div className="upcomingEvent-container">
+        <h1 className="section-title">UPCOMING EVENTS</h1>
+        <p className="upcomingEvent-content">
+          {Config.upcomingEvent} <a href={Config.membership}>-DETAILS-</a>
+        </p>
+        <p className="upcomingEvent-content">
+          {Config.upcomingEvent2} <a href={Config.membership}>-DETAILS-</a>
+        </p>
+        <p className="upcomingEvent-content">
+          {Config.upcomingEvent3} <a href={Config.membership}>-DETAILS-</a>
+        </p>
+        <p className="upcomingEvent-content upcomingEvent-subcontent">
+          <a href={Config.membership}>-INFORMATION-</a> Sign up by ringing or
+          texting 0274063188
+        </p>
+      </div>
+    </div>
+  );
+};
 
-    return (        
-        <div id="upcomingEvent" className="upcomingEvent" >
-            <div className="upcomingEvent-container">
-                <h1 className="section-title">UPCOMING EVENTS</h1>
-                <p className="upcomingEvent-content">{Config.upcomingEvent} <a href={Config.membership}>-DETAILS-</a></p>
-                <p className="upcomingEvent-content upcomingEvent-subcontent"><a href={Config.membership}>-INFORMATION-</a> Sign up by ringing or texting 0274063188</p>
-            </div>
-        </div>         
-    )
-}
-
-export default UpcomingEvent
+export default UpcomingEvent;
